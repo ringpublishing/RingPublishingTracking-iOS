01fabc7a10d6dc5e9bbac45444b1239f690855d0
Adam Szeremeta <adam.szeremeta@ringieraxelspringer.pl>
2021-09-17 13:49:04 +0200

    Demo app pager with two controllers

---
+++ Example/AppTracking/Controller/ActionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ActionsViewController.swift
+//  AppTracking-Example
+//
+//  Created by Adam Szeremeta on 17/09/2021.
+//  Copyright © 2021 Ringier Axel Springer Tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ActionsViewController: UIViewController, PagerViewController {
+
+    var pageIndex: Int {
+        return 1
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ Example/AppTracking/Controller/ListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ListViewController.swift
+//  AppTracking-Example
+//
+//  Created by Adam Szeremeta on 17/09/2021.
+//  Copyright © 2021 Ringier Axel Springer Tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ListViewController: UIViewController, PagerViewController {
+
+    var pageIndex: Int {
+        return 0
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
