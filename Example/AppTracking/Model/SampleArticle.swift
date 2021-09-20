5a9265127617ea9361dd798083fa492e522141f5
Adam Szeremeta <adam.szeremeta@ringieraxelspringer.pl>
2021-09-20 10:05:16 +0200

    multiline text

--- Example/AppTracking/Model/SampleArticle.swift
+++ Example/AppTracking/Model/SampleArticle.swift
@@ -25,13 +25,17 @@ private extension SampleArticle {
         return UUID().uuidString
     }
 
-    // swiftlint:disable line_length
     static func generateArticleContent() -> [String] {
-        let sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        let sampleText =
+        """
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        """
 
         return Array(repeating: sampleText, count: 10)
     }
-    // swiftlint:enable line_length
 }
 
 // swiftlint:disable force_unwrapping
