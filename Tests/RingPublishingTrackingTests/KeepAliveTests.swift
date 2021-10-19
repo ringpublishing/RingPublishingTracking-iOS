b5bd140a36d7672cb676068a86fd36d692a68567
Artur Rymarz <artur.rymarz@ringieraxelspringer.pl>
2021-10-19 13:42:34 +0200

    Added tests for Intervals Providers

---
+++ Tests/RingPublishingTrackingTests/KeepAliveTests.swift
@@ -0,0 +1,21 @@
+//
+//  KeepAliveTests.swift
+//  RingPublishingTrackingTests
+//
+//  Created by Artur Rymarz on 19/10/2021.
+//  Copyright © 2021 Ringier Axel Springer Tech. All rights reserved.
+//
+
+import XCTest
+
+class KeepAliveTests: XCTestCase {
+
+    // MARK: Setup
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    // MARK: Tests
+    
+}
