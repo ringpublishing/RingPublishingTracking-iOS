1b5164d7cce1a24a1bca09f3ccc88551ccda6155
Artur Rymarz <artur.rymarz@ringieraxelspringer.pl>
2021-10-27 09:44:30 +0200

    Added ConsentProviderTests

---
+++ Tests/RingPublishingTrackingTests/ConsentProviderTests.swift
@@ -0,0 +1,32 @@
+//
+//  ConsentProviderTests.swift
+//  RingPublishingTrackingTests
+//
+//  Created by Artur Rymarz on 27/10/2021.
+//  Copyright © 2021 Ringier Axel Springer Tech. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+
+class ConsentProviderTests: XCTestCase {
+
+    // MARK: Setup
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    // MARK: Tests
+    func testAdpc_IABTCF_TCStringNotFilled_noValueReturned() {
+        // Given
+        let provider = ConsentProvider()
+
+        // When
+        let adpc = provider.adpc
+
+        // Then
+        XCTAssertNil(adpc, "adpc should be nil")
+    }
+}
