927ff44305b901f3bc4e40ef8122a7f494071d34
Bernard Bijoch <bernard.bijoch@ringieraxelspringer.pl>
2024-09-04 14:16:06 +0200

    RDLCN as a computed property on ContentMetadata

--- Sources/RingPublishingTracking/Private/Extensions/ContentMetadata+Parameters.swift
+++ Sources/RingPublishingTracking/Private/Extensions/ContentMetadata+Parameters.swift
@@ -1,5 +1,5 @@
 //
-//  ContentMetadata+DX.swift
+//  ContentMetadata+Parameters.swift
 //  RingPublishingTracking
 //
 //  Created by Artur Rymarz on 14/10/2021.
@@ -10,6 +10,12 @@ import Foundation
 
 extension ContentMetadata {
 
+    private static var encoder = {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .sortedKeys
+        return encoder
+    }()
+
     var dxParameter: String {
         let sourceSystem = sourceSystemName.trimmingCharacters(in: .whitespacesAndNewlines)
         let pubId = publicationId.trimmingCharacters(in: .whitespacesAndNewlines)
@@ -17,5 +23,16 @@ extension ContentMetadata {
         let paid = paidContent ? "t" : "f"
 
         return "PV_4,\(sourceSystem),\(pubId),\(part),\(paid)".replacingOccurrences(of: " ", with: "_")
+    }
+
+    var rdlcnParameter: String? {
+        let contentMarkAsPaid = ContentMarkAsPaid(contentMetadata: self)
+
+        if let jsonData = try? Self.encoder.encode(contentMarkAsPaid),
+           let jsonString = String(data: jsonData, encoding: .utf8) {
+            return Data(jsonString.utf8).base64EncodedString()
+        }
+
+        return nil
     }
 }
